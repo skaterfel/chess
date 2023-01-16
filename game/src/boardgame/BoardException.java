30cda43e96d6475f52851239c18b5e31b8252caf
Felipe Melo <felipe.leandro2@gmail.com>
2023-01-16 14:06:30 -0300

    Moving pieces

--- game/src/boardgame/BoardException.java
+++ game/src/boardgame/BoardException.java
@@ -1,10 +1,10 @@
 package boardgame;
 
-public class BoardExcepition extends RuntimeException{
+public class BoardException extends RuntimeException{
 
 	private static final long serialVersionUID = 1L;
 	
-	public BoardExcepition(String msg) {
+	public BoardException(String msg) {
 		super(msg);
 	}
 
