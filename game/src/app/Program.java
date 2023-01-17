a36c7c9b6f3482b637012cb6c0b738eeaa09e5b0
Felipe Melo <felipe.leandro2@gmail.com>
2023-01-16 23:26:49 -0300

    Possible moves of a piece

--- game/src/app/Program.java
+++ game/src/app/Program.java
@@ -15,6 +15,7 @@ public class Program {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		ChessMatch chessMatch = new ChessMatch();
+		System.out.println("corrigindo commit anterior");
 		
 		while(true) {
 			try {
